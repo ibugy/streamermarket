cfdf4b98a0c172d00d2843ef5dc22382b9a8edc4
Darío <darioabenzamorales@gmail.com>
2022-06-19 20:24:16 +0200

    Project creation catjam and wokege

---
+++ catjam/src/main/java/com/ibugy/catjam/CatjamApplication.java
@@ -0,0 +1,13 @@
+package com.ibugy.catjam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatjamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatjamApplication.class, args);
+	}
+
+}
