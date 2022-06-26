f40bb6a43e0e480bc196e9f413b1f3b22cba795a
Darío <darioabenzamorales@gmail.com>
2022-06-26 16:46:29 +0200

    jobs rework

--- wokege/src/main/java/com/ibugy/wokege/WokegeApplication.java
+++ wokege/src/main/java/com/ibugy/wokege/WokegeApplication.java
@@ -3,11 +3,8 @@ package com.ibugy.wokege;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import com.ibugy.wokege.job.StreamDataCollector;
 
 @SpringBootApplication
 @EnableScheduling
@@ -16,8 +13,6 @@ import com.ibugy.wokege.job.StreamDataCollector;
 public class WokegeApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(WokegeApplication.class, args);
-		StreamDataCollector collectorThread = context.getBean(StreamDataCollector.class);
-		collectorThread.run();
+		SpringApplication.run(WokegeApplication.class, args);
 	}
 }
--- wokege/src/main/java/com/ibugy/wokege/job/StreamDataCollector.java
+++ wokege/src/main/java/com/ibugy/wokege/job/StreamDataCollector.java
@@ -3,90 +3,53 @@ package com.ibugy.wokege.job;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Set;
+
+import javax.annotation.PostConstruct;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Service;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+import com.ibugy.streamermarket.common.model.DailyStreamData;
+import com.ibugy.streamermarket.common.repository.DailyStreamDataRepository;
 import com.ibugy.streamermarket.common.repository.StreamerRepository;
 import com.ibugy.wokege.twitch.business.TwitchBusiness;
 import com.ibugy.wokege.twitch.model.TwitchStreamInfo;
 import com.ibugy.wokege.twitch.model.TwitchStreamsData;
 
-/**
- * Runs a job that collects data from the twitch API for specific streamers listed in a file. The data is stored in a map with the name of
- * the streamer as a key, and a list of stream infos as value. This data can be retrieved with the method {@link #getCollectedData()}
- */
-@Service
-@Scope("singleton")
-public class StreamDataCollector implements Runnable {
+@Component
+public class StreamDataCollector {
 
 	private static final Log LOG = LogFactory.getLog(StreamDataCollector.class);
 	@Autowired
 	private TwitchBusiness twitchBusiness;
 	@Autowired
 	private StreamerRepository streamerRepository;
+	@Autowired
+	private DailyStreamDataRepository streamRepository;
 	private HashMap<String, ArrayList<TwitchStreamInfo>> twitchStreamsCollectedData;
-	private boolean running;
-	private static final long FREQUENCY = 10 * 1000; // 10 seconds
 	private static final long MIN_LIVE_TIME = 1000 * 60 * 10; // 10 minutes
 
-	@Override
-	public void run() {
-		running = true;
-		LOG.info("**** STREAM DATA COLLECTOR STARTED ****");
+	@PostConstruct
+	public void postConstruct() {
 		twitchStreamsCollectedData = new HashMap<>();
-		// !- Get streamers to search for
 		streamerRepository.findAll()
 			.stream()
 			.forEach(streamer -> {
 				twitchStreamsCollectedData.put(streamer.getName(), new ArrayList<>());
 			});
-		// !- Collect stream data from twitch indefinitely to be collected by the StreamDataProcessor
-		while (running) {
-			long timeBeforeTask = new Date().getTime();
-			long timeNextIteration = timeBeforeTask + FREQUENCY;
-			collectData();
-			long timeAfterTask = new Date().getTime();
-			long timeToSleep = timeNextIteration - timeAfterTask;
-			if (timeToSleep < 0) {
-				LOG.warn("WARNING: Collect task is taking more time than the set frequency");
-			} else {
-				try {
-					Thread.sleep(timeToSleep);
-				} catch (InterruptedException e) {
-					LOG.error(e.getMessage(), e);
-				}
-			}
-		}
 	}
 
-	/**
-	 * Returns the collected data. It is advised to use {@link #stop()} before collecting and processing the data, to avoid access
-	 * conflicts. The process can be restarted after, using {@link #run()}
-	 * 
-	 * @return
-	 */
-	public HashMap<String, ArrayList<TwitchStreamInfo>> getCollectedData() {
-		return twitchStreamsCollectedData;
-	}
-
-	/**
-	 * Stops the process. It can be restarted after, using {@link #run()}
-	 */
-	public void stop() {
-		LOG.info("**** STREAM DATA COLLECTOR STOPPED ****");
-		running = false;
-	}
-
+	@Scheduled(cron = "${wokege.job.stream.data.collector}")
 	private void collectData() {
+		LOG.info("**** STREAM DATA COLLECTOR STARTED ****");
 		TwitchStreamsData data = twitchBusiness.getStreamInfo(twitchStreamsCollectedData.keySet()
 			.toArray(String[]::new));
-		List<TwitchStreamInfo> streamsInfo = data.getData();
-		streamsInfo.stream()
+		data.getData()
+			.stream()
 			.forEach(streamInfo -> {
 				String streamer = streamInfo.getUserLogin();
 				// !- Will only add the stream if it's been live for more than MIN_LIVE_TIME
@@ -97,5 +60,33 @@ public class StreamDataCollector implements Runnable {
 					twitchStreamsCollectedData.get(streamer)
 						.add(streamInfo);
 			});
+		LOG.info("**** STREAM DATA COLLECTOR FINISHED ****");
+	}
+
+	@Scheduled(cron = "${wokege.job.stream.data.processor}")
+	public void processData() {
+		LOG.info("**** STREAM DATA PROCESSOR STARTED ****");
+		ArrayList<DailyStreamData> streamsToSave = new ArrayList<>();
+		Set<String> streamers = twitchStreamsCollectedData.keySet();
+		streamers.stream()
+			.forEach(streamer -> {
+				ArrayList<TwitchStreamInfo> streams = twitchStreamsCollectedData.get(streamer);
+				if (streams.size() > 0) {
+					int viewsSum = 0;
+					int streamsCount = 0;
+					int avgViews;
+					for (TwitchStreamInfo streamInfo : streams) {
+						viewsSum += streamInfo.getViewerCount();
+						streamsCount++;
+					}
+					avgViews = viewsSum / streamsCount;
+					LOG.info("Streamer " + streamer + " had " + avgViews + "average views today");
+					streamsToSave.add(new DailyStreamData(new Date(), streamer, avgViews));
+				}
+			});
+		// ! - Calculate average views last 7 days and adjust streamer coinValue
+		// TODO
+		streamRepository.saveAll(streamsToSave);
+		LOG.info("**** STREAM DATA PROCESSOR FINISHED ****");
 	}
 }
